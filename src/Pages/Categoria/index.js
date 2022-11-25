3a10574a76eac88ca4c3ce62621907da6aa54faf
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-11-25 12:10:55 -0300

    Pegando itens

--- src/Pages/Categoria/index.js
+++ src/Pages/Categoria/index.js
@@ -1,13 +1,17 @@
 import Header from 'components/Header';
 import React from 'react';
+import styles from './Categoria.module.scss';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 export default function Categoria() {
   const { nomeCategoria } = useParams();
-  const categoria = useSelector((state) =>
-    state.categorias.find((categoria) => categoria.id === nomeCategoria)
-  );
+  const { categoria, itens } = useSelector((state) => {
+    return {
+      categoria: state.categorias.find((categoria) => categoria.id === nomeCategoria),
+      itens: state.itens.filter((item) => item.categoria === nomeCategoria),
+    };
+  });
 
   return (
     <div>
@@ -16,6 +20,11 @@ export default function Categoria() {
         descricao={categoria.descricao}
         imagem={categoria.header}
       />
+      <div className={styles.itens}>
+        {itens?.map((item) => (
+          <div key={item.id}>{item.titulo}</div>
+        ))}
+      </div>
     </div>
   );
 }
